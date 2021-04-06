8a2a02ef472f0b14ca119c2febe69ecda2060d53
Ruimas-UTI <allpraisethelord@gmail.com>
2021-04-06 08:38:43 +0600

    homepage init

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Directory from '../../components/directory/directory.componets'
+import './homepage.styles.scss';
+export default function Homepage() {
+    return (
+  
+     <div className='homepage'>
+            <Directory/>
+    </div>
+    )
+}
