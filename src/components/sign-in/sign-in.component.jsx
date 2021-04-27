30ec7300fabd514d078874d6d77a317be59162de
Ruimas-UTI <allpraisethelord@gmail.com>
2021-04-27 14:42:00 +0600

    sign-in form init

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -8,15 +8,26 @@ export default class SignIn extends Component {
             password:''
         }
     }
+    handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(e.target.value)
+    }
+    handleChange = (event) => {
+        const {name,value} = event.target;
+        this.setState({
+            [name] : value
+        })
+        console.log(this.state);
+    }
     render() {
         return (
             <div className='sign-in'>
                 <h2>I already have an account</h2>
                 <span>Sign in with your email and password</span>
-                <form>
-                    <input name='email' type='email' value={this.state.email} required/>
+                <form onSubmit={this.handleSubmit}>
+                    <input name='email' type='email' onChange ={this.handleChange} value={this.state.email} required/>
                     <label>Email</label>
-                    <input name='password' type='password' value={this.state.password}/>
+                    <input name='password' type='password' onChange ={this.handleChange} value={this.state.password}/>
                     <input type="submit" value='submit form'/>
                 </form>
             </div>
