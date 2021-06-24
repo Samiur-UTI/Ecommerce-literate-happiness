81d84c24f17477af40f868d5528328d6b8276691
Ruimas-UTI <allpraisethelord@gmail.com>
2021-06-25 00:31:07 +0600

    signup error

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -27,7 +27,7 @@ export default class SignIn extends Component {
                 <h2>I already have an account</h2>
                 <span>Sign in with your email and password</span>
                 <form onSubmit={this.handleSubmit}>
-                    <FormInput name='email' type='email' label='email' handleChange ={this.handleChange} value={this.state.email} required/>
+                    <FormInput name='email' type='email' label='email' onChange ={this.handleChange} value={this.state.email} required/>
                     <FormInput name='password' type='password' label='password' onChange ={this.handleChange} value={this.state.password}/>
                     <div className='buttons'>
                         <CustomButton type="submit" value='submit form'>Sign In</CustomButton>
