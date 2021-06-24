79db4a2e1590173d7d07c8eb3cd51c2ce6e82d70
Ruimas-UTI <allpraisethelord@gmail.com>
2021-06-25 03:13:54 +0600

    auth done

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import FormInput from "../form-input/form-input.component";
 import './sign-in.style.scss';
 import CustomButton from '../custom-button/custom-button.component';
-import {signInWithGoogle} from '../../firebase/firebase.utils';
+import {auth,signInWithGoogle} from '../../firebase/firebase.utils';
 
 export default class SignIn extends Component {
     constructor(props){
@@ -12,9 +12,15 @@ export default class SignIn extends Component {
             password:''
         }
     }
-    handleSubmit = (e) => {
+    handleSubmit = async (e) => {
         e.preventDefault();
-        console.log(e)
+        const {email, password} = this.state
+        try {
+            await auth.signInWithEmailAndPassword(email, password)
+            this.setState({email:'', password:''})
+        } catch (err) {
+            console.error(err)
+        }
     }
     handleChange = (event) => {
         const {name,value} = event.target;
